8902ad90a97d8d2ba83d54160e5cc01e46fa91b2
ffgamer1080-pixel <ffgamer1080@gmail.com>
2026-07-05 13:02:01 +0530

    Add lyric timeline for the lyric engine

---
+++ lyrics.js
@@ -0,0 +1,23 @@
+// Chronological timeline script for the lyric engine
+const lyricTimeline = [
+    {
+        time: 2.5,
+        hindi: "तन्ने मैं लिखूँ ",
+        english: "RANI DIL KI"
+    },
+    {
+        time: 6.8,
+        hindi: "YA TANNE ",
+        english: "MUMTAZ लिखूँगा"
+    },
+    {
+        time: 11.2,
+        hindi: "JE लिखने में बैठ गया TANNE",
+        english: ""
+    },
+    {
+        time: 14.5,
+        hindi: "TO PAKKA मैं ",
+        english: "KITAB लिखूँगा"
+    }
+];
