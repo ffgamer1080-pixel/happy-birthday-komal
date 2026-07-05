bf2b1ef25f77e1a8ce75e8848a5a4f40dd025931
ffgamer1080-pixel <ffgamer1080@gmail.com>
2026-07-05 16:43:26 +0530

    Refactor particle animation and properties

--- particles.js
+++ particles.js
@@ -4,9 +4,13 @@ constructor(){
 
 this.canvas=document.getElementById("particleCanvas");
 
+if(!this.canvas)return;
+
 this.ctx=this.canvas.getContext("2d");
 
 this.particles=[];
+
+this.running=false;
 
 this.resize();
 
@@ -14,7 +18,7 @@ window.addEventListener("resize",()=>this.resize());
 
 this.createParticles();
 
-this.animate();
+this.start();
 
 }
 
@@ -28,7 +32,9 @@ this.canvas.height=window.innerHeight;
 
 createParticles(){
 
-for(let i=0;i<60;i++){
+this.particles=[];
+
+for(let i=0;i<100;i++){
 
 this.particles.push({
 
@@ -36,55 +42,44 @@ x:Math.random()*this.canvas.width,
 
 y:Math.random()*this.canvas.height,
 
-size:Math.random()*5+2,
+size:Math.random()*4+2,
 
-speedX:(Math.random()-0.5)*1.2,
+speedX:(Math.random()-0.5)*0.8,
 
-speedY:Math.random()*1+0.5,
+speedY:Math.random()*1.2+0.4,
+
+opacity:Math.random()*0.5+0.3,
 
 angle:Math.random()*360,
 
-rotate:Math.random()*2-1,
+rotation:(Math.random()-0.5)*2,
 
-opacity:Math.random()*0.5+0.4
+color:Math.random()>0.5?"#FFD700":"#FF4D6D"
 
 });
 
 }
 
 }
-
 draw(){
 
 this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
 
 this.particles.forEach(p=>{
 
-p.x+=p.speedX;
-
-p.y+=p.speedY;
-
-p.angle+=p.rotate;
-
-if(p.y>this.canvas.height+20){
-
-p.y=-20;
-
-p.x=Math.random()*this.canvas.width;
-
-}
-
 this.ctx.save();
+
+this.ctx.globalAlpha=p.opacity;
 
 this.ctx.translate(p.x,p.y);
 
 this.ctx.rotate(p.angle*Math.PI/180);
 
-this.ctx.fillStyle=`rgba(255,77,109,${p.opacity})`;
+this.ctx.fillStyle=p.color;
 
 this.ctx.beginPath();
 
-this.ctx.ellipse(0,0,p.size,p.size/1.5,0,0,Math.PI*2);
+this.ctx.arc(0,0,p.size,0,Math.PI*2);
 
 this.ctx.fill();
 
@@ -96,12 +91,65 @@ this.ctx.restore();
 
 animate(){
 
+if(!this.running)return;
+
 this.draw();
+
+this.particles.forEach(p=>{
+
+p.x+=p.speedX;
+
+p.y+=p.speedY;
+
+p.angle+=p.rotation;
+
+if(p.y>this.canvas.height+20){
+
+p.y=-20;
+
+p.x=Math.random()*this.canvas.width;
+
+}
+
+if(p.x<-20){
+
+p.x=this.canvas.width+20;
+
+}
+
+if(p.x>this.canvas.width+20){
+
+p.x=-20;
+
+}
+
+});
 
 requestAnimationFrame(()=>this.animate());
 
 }
+start(){
+
+if(!this.canvas || !this.ctx)return;
+
+if(this.running)return;
+
+this.running=true;
+
+this.animate();
 
 }
 
+stop(){
+
+this.running=false;
+
+}
+
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+
 new ParticleEngine();
+
+});
